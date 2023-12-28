443564420212bf533bcb091ad178000004f4511e
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-12-29 02:34:10 +0900

    更改应用名，添加 polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import path from "path";
 import px2rem from "postcss-plugin-px2rem";
 import postcssPresetEnv from "postcss-preset-env";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import { BuildOptions, defineConfig } from "vite";
 import tailwindConfig from "./tailwind.config.js";
 import AutoImport from "unplugin-auto-import/vite";
 import { ArcoResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import vue from "@vitejs/plugin-vue";
 import { vitePluginForArco } from "@arco-plugins/vite-vue";
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -25,7 +26,25 @@ export default defineConfig(async () => ({
     vitePluginForArco({
       style: "css",
     }),
+    legacy({
+      targets: [
+        'Android >= 39',
+        'Chrome >= 50',
+        'Safari >= 10.1',
+        'iOS >= 10.3',
+        '> 1%',
+      ],
+    }),
   ],
+  build: {
+    minify: "terser",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 2000,
+    terserOptions: {
+      toplevel: true,
+      safari10: false,
+    },
+  } as BuildOptions,
   css: {
     postcss: {
       plugins: [
@@ -57,4 +76,5 @@ export default defineConfig(async () => ({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  
 }));
